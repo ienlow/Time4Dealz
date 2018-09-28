6e3d3d8bd2217cccd45b48392262732f6ea41a4b
Isaac Enlow <ienlow@radford.edu>
2018-09-28 19:25:48 -0400

    changed main_menu page and rewards page

--- app/src/main/java/com/example/isaacenlow/time4dealz/Rewards.java
+++ app/src/main/java/com/example/isaacenlow/time4dealz/Rewards.java
@@ -3,6 +3,8 @@ package com.example.isaacenlow.time4dealz;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 import java.util.prefs.Preferences;
@@ -10,14 +12,25 @@ import java.util.prefs.Preferences;
 public class Rewards extends AppCompatActivity {
     ProgressBar progressBar;
     SharedPreferences preferences;
+    Button redeemReward500, redeemReward1000;
     public static final String MY_PREFS = "MyPrefs";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rewards);
+        redeemReward500 = findViewById(R.id.redeemReward500);
+        redeemReward1000 = findViewById(R.id.redeemReward1000);
         progressBar = findViewById(R.id.rewardsProgress);
         preferences = getSharedPreferences(MY_PREFS, 0);
+        int points = preferences.getInt("points", 0);
         progressBar.setProgress(preferences.getInt("points", 0));
+
+        if (points >= R.integer.Reward500) {
+            redeemReward500.setVisibility(View.VISIBLE);
+        }
+        else if (points >= R.integer.Reward1000) {
+            redeemReward1000.setVisibility(View.VISIBLE);
+        }
     }
 }
