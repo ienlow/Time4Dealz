5a5406aa4ff40e9743336cd8f465b846d4da536f
ienlow <isaac.enlow@gmail.com>
2018-09-29 21:20:44 -0400

    updated main menu and rewards page

--- app/src/main/java/com/example/isaacenlow/time4dealz/Rewards.java
+++ app/src/main/java/com/example/isaacenlow/time4dealz/Rewards.java
@@ -1,16 +1,18 @@
 package com.example.isaacenlow.time4dealz;
 
 import android.content.SharedPreferences;
+import android.graphics.ColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.prefs.Preferences;
 
 public class Rewards extends AppCompatActivity {
-    ProgressBar progressBar;
+    ProgressBar progressBar500, progressBar1000;
     SharedPreferences preferences;
     Button redeemReward500, redeemReward1000;
     public static final String MY_PREFS = "MyPrefs";
@@ -21,15 +23,17 @@ public class Rewards extends AppCompatActivity {
         setContentView(R.layout.rewards);
         redeemReward500 = findViewById(R.id.redeemReward500);
         redeemReward1000 = findViewById(R.id.redeemReward1000);
-        progressBar = findViewById(R.id.rewardsProgress);
+        progressBar500 = findViewById(R.id.rewardsProgress500);
+        progressBar1000 = findViewById(R.id.rewardsProgress1000);
         preferences = getSharedPreferences(MY_PREFS, 0);
         int points = preferences.getInt("points", 0);
-        progressBar.setProgress(preferences.getInt("points", 0));
+        progressBar500.setProgress(points);
+        progressBar1000.setProgress(points);
 
-        if (points >= R.integer.Reward500) {
+        if (points >= 20) {
             redeemReward500.setVisibility(View.VISIBLE);
         }
-        else if (points >= R.integer.Reward1000) {
+        if (points >= 100) {
             redeemReward1000.setVisibility(View.VISIBLE);
         }
     }
